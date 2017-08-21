042472ad61779fbb1e1f40600d623f652595100c
Marina Sherstuk <marinas@commpeak.com>
2017-08-21 14:48:12 +0300

    fixed IParams to IReturnValue

--- src/Narrator/IReturnValue.php
+++ src/Narrator/IReturnValue.php
@@ -7,38 +7,38 @@ interface IReturnValue
 	/**
 	 * @param string $type
 	 * @param callable|mixed $value
-	 * @return IParams
+	 * @return IReturnValue
 	 */
-	public function byType(string $type, $value): IParams;
+	public function byType(string $type, $value): IReturnValue;
 	
 	/**
 	 * @param array $valueByType
-	 * @return IParams
+	 * @return IReturnValue
 	 */
-	public function byTypes(array $valueByType): IParams;
+	public function byTypes(array $valueByType): IReturnValue;
 	
 	/**
 	 * @param string $subType
 	 * @param callable|mixed $value
-	 * @return IParams
+	 * @return IReturnValue
 	 */
-	public function bySubType(string $subType, $value): IParams;
+	public function bySubType(string $subType, $value): IReturnValue;
 	
 	/**
 	 * @param array $valueBySubType
-	 * @return IParams
+	 * @return IReturnValue
 	 */
-	public function bySubTypes(array $valueBySubType): IParams;
+	public function bySubTypes(array $valueBySubType): IReturnValue;
 	
 	/**
 	 * @param callable|mixed $value
-	 * @return IParams
+	 * @return IReturnValue
 	 */
-	public function defaultValue($value): IParams;
+	public function defaultValue($value): IReturnValue;
 	
 	/**
 	 * @param callable|mixed $value
-	 * @return IParams
+	 * @return IReturnValue
 	 */
-	public function onNull($value): IParams;
+	public function onNull($value): IReturnValue;
 }
